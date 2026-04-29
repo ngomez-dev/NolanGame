738ac43b6ca6746012b05dfdad8ad5378bd71a73
ngomez-dev <ngomez@gocfs.net>
2026-04-28 23:32:39 -0400

    Update RealGame.py

--- RealGame.py
+++ RealGame.py
@@ -19,14 +19,17 @@ BLACK = (0, 0, 0)
 # Platform
 platform = pygame.Rect(350, 550, 100, 10)
 
+
 # Ball
 ball = pygame.Rect(390, 300, 20, 20)
+ball_x_speed = 4
+ball_y_speed = -4
 
 
 # Bricks
 bricks = []
-for i in range(5):  # rows
-    for j in range(10):  # columns
+for i in range(5):
+    for j in range(10):
         brick = pygame.Rect(j * 80 + 5, i * 30 + 5, 70, 20)
         bricks.append(brick)
 
@@ -35,11 +38,32 @@ score = 0
 running = True
 
 while running:
+
+#Events/Keys
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        platform.x = platform.x - 6
+    if keys[pygame.K_RIGHT]:
+        platform.x = platform.x + 6
+# Ball Movement
+    ball.x = ball.x + ball_x_speed
+    ball.y = ball.y + ball_y_speed
+
+
+
     screen.fill(BLACK)
 
-
+# Drawings
     pygame.draw.rect(screen, BLUE, platform)
     pygame.draw.ellipse(screen, WHITE, ball)
+    for brick in bricks:
+        pygame.draw.rect(screen,(RED), brick)
+
 
 
 
@@ -47,5 +71,24 @@ while running:
 
     pygame.display.update()
     clock.tick(60)
+
+# Ball Colliding
+    if ball.colliderect(platform):
+        ball_y_speed=-ball_y_speed
+
+
+
+    for brick in bricks:
+        if ball.colliderect(brick):
+            ball_y_speed = -ball_y_speed
+            score = score + 1
+            bricks.remove(brick)
+
+
+
+
+
+
+
 
 pygame.quit()
